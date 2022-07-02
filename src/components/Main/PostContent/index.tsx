9223a9cd2cb731c2176b82abb5a9b6aaafcfa5c7
João Vitor Marra <93348783+joaomarr@users.noreply.github.com>
2022-07-02 12:35:53 -0300

    Update mediaqueries

--- src/components/Main/PostContent/index.tsx
+++ src/components/Main/PostContent/index.tsx
@@ -42,7 +42,7 @@ export function PostContent({...data}) {
 
                     <Aside />
 
-                    <div className="post-rendered text-2xl bg-white text-gray-900 p-8">
+                    <div className="post-rendered bg-white text-gray-900 p-8">
                         {parser(data.content.rendered)}
                     </div>
                 </div>
